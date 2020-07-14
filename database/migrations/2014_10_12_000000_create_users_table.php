239c71796bea7848b9cca103f5126d869f8ceb99
latif <abdoulatifshaban@gmail.com>
2020-07-14 22:04:56 +0200

    set langage default

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email',100)->unique();
+            $table->string('langage')->default('Francais');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('university');
